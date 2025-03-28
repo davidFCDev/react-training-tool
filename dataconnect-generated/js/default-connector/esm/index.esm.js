0acd323cc03ea8141fde626bd6495665844e273e
David Fernández Comesañá <davidfernandezcomesana@gmail.com>
2025-03-28 13:02:31 +0100

    deploy updated

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'react-training-ai',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'react-training-ai',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
